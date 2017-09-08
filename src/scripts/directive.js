1f96a5970d6487030842a1b807a58521432a390b
fanpeiyao <952568789@qq.com>
2017-09-08 09:04:00 +0800

    geng gai sou suo

--- src/scripts/directive.js
+++ src/scripts/directive.js
@@ -1,6 +1,6 @@
 
 angular.module('myApp')
-    .directive('listActive',function () {
+    .directive('listActive',['$window',function ($window) {
         return {
             restrict:'A',
             scope:{
@@ -9,11 +9,13 @@ angular.module('myApp')
             link:function (scope,element) {
                 element.on('click',function () {
                     var detail_target = angular.element(this).attr('data-id');
+                    var ty = $window.innerHeight/2 - element[0].offsetTop -4;
+                    angular.element(this).attr('style',"transform: translateY(" + ty + "px)");
                     var de = document.getElementById(detail_target);
                     de.setAttribute('class','active list-detail')
                     angular.element(this).find('.md-card-image').addClass('aa');
                 })
             }
         }
-    })
+    }])
 
