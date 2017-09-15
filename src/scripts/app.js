1bf647c201742fe00d81337b19c71e716ba9697e
fanpeiyao <952568789@qq.com>
2017-09-15 11:02:58 +0800

    0915--1

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,6 +4,7 @@ var myApp = angular.module("myApp",['ngMaterial','ngAria','ngAnimate'])
 
         var con = document.getElementsByClassName('content')[0];
         con.style.minHeight=$window.innerHeight +'px';
+        var lists = document.getElementsByClassName('son-list'),time;
         // 样式
         $scope.closeDe = function (id) {
             var id = 'active_'+id;
@@ -45,7 +46,6 @@ var myApp = angular.module("myApp",['ngMaterial','ngAria','ngAnimate'])
             imgLoader(imgList, function (percentage) {
                 var percentT = percentage * 100;
                 $('#percent').html((parseInt(percentT)) + '%');
-
                 if (parseInt(percentT) == 100) {
                     $("#loading").hide();
                     $("#coverPic").fadeIn(1000);
@@ -64,16 +64,8 @@ var myApp = angular.module("myApp",['ngMaterial','ngAria','ngAnimate'])
                 var newUser = [];
                 $scope.users = [];
                 newUser = ABCSort(users);
-                var lists = document.getElementsByTagName('md-list'),time;
-                //console.log(newUser.length)
-                //console.log(lists.length)
-                for (var i=0;i<newUser.length;i++){
-                    $scope.users.push(newUser[i]);
-                    //Cannot read property 'style' of undefined,本来lists[i]应该单独写循环，虽报错不影响
-                    time = (i*100)+'ms';
-                    lists[i].style.animationDelay = time;
-                };
 
+                forLists(newUser);
 
                 //实现查询功能
                 $scope.$watch('searchText', function(searchText) {
@@ -85,13 +77,51 @@ var myApp = angular.module("myApp",['ngMaterial','ngAria','ngAnimate'])
                     }
                 });
             }
-
+            function forLists(newUser) {
+                for (var i=0;i<newUser.length;i++){
+                    console.log($scope.users)
+                    $scope.users.push(newUser[i]);
+                    time = (i*100+100)+'ms';
+                    lists[i].style.animationDelay = time;
+                };
+            }
             //排序切换
             $(".switchSort").click(function(){
+
                 var flag=$(this).attr("sort");
                 if(flag=="groupName"){
-                    $scope.users=strSort(users,flag);
-                    console.log($scope.users)
+                    var data= strSort(users,flag);
+                    var datalist=[];
+                    for(var i=0;i<data.length;i++){
+                        datalist.push(data[i].details[0].team)
+                    }
+                    function sortarr(arrlist,dataSort){
+                        for(i=0;i<arrlist.length-1;i++){
+                            for(j=0;j<arrlist.length-1-i;j++){
+                                if(arrlist[j]>arrlist[j+1]){
+                                    var temp=arrlist[j];
+                                    arrlist[j]=arrlist[j+1];
+                                    arrlist[j+1]=temp;
+
+                                    var sortdata=dataSort[j];
+                                    dataSort[j]=dataSort[j+1];
+                                    dataSort[j+1]=sortdata;
+                                }
+                            }
+                        }
+                        return dataSort;
+                    }
+                    $scope.users=sortarr(datalist,data);
+                    var sortUser = [];
+
+                    // sortUser=sortarr(datalist,data);
+                    //
+                    // forLists(sortUser);
+                    //console.log($scope.users)
+
+
+                    //time = (i*100+100)+'ms';
+                    //lists[i].style.animationDelay = time;
                 }
                 if(flag=="ABC"){
                     $scope.users=ABCSort(users);
@@ -100,5 +130,4 @@ var myApp = angular.module("myApp",['ngMaterial','ngAria','ngAnimate'])
             })
 
         })
-
     }])
