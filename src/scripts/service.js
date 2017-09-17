f2bbeb47cbfc6fe361c2e70f17aeebf5b64f9b11
fanpeiyao <952568789@qq.com>
2017-09-17 17:50:27 +0800

    排序按循环，更改部分代码

--- src/scripts/service.js
+++ src/scripts/service.js
@@ -1,15 +1,39 @@
 
 
 angular.module("myApp")
-    .factory("ManService",function ($http,$q) {
+    .service("ManService",function ($http,$q) {
 
-        var q=$q.defer();
-        var obj=q.promise;
+        this.getAll = function () {
+            //通过$q服务注册一个延迟对象 q
+            var q=$q.defer();
+            //通过q，可以得到一个承诺promise，而promise会返回当前任务的完成结果
+            var obj=q.promise;
 
-        $http({url:"../resource/resource.json",method:"get"}).then(function (e) {
-            q.resolve(e);
-        })
-        return obj;
+            $http({url:"../resource/resource.json",method:"get"}).then(function (e) {
+                q.resolve(e);
+            });
+
+            return obj;
+        };
+        //筛选
+        this.getSort = function () {
+            var sorts = [
+                {
+                    name:'groupName',
+                    iconName:'contacts',
+                    tag:''
+                },
+                {
+                    name:'ABC',
+                    iconName:'sort_by_alpha',
+                    pink:true,
+                    tag:''
+                }
+            ];
+            return sorts;
+        }
+
+
 
     })
 
