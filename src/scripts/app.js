615bccbd2776e3a4e54f0ddd5056eab179722e25
chentzju <chent.zju@gmail.com>
2017-09-21 10:43:43 +0800

    优化了排序功能

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -94,27 +94,30 @@ var myApp = angular.module("myApp",['ngMaterial','ngAria','ngAnimate'])
             if(name=='groupName'){
                 var flag=name;
                 var data= strSort(users,flag);
-                var datalist=[];
-                for(var i=0;i<data.length;i++){
-                    datalist.push(data[i].details[0].team)
-                }
-                function sortarr(arrlist,dataSort){
-                    for(i=0;i<arrlist.length-1;i++){
-                        for(j=0;j<arrlist.length-1-i;j++){
-                            if(arrlist[j]>arrlist[j+1]){
-                                var temp=arrlist[j];
-                                arrlist[j]=arrlist[j+1];
-                                arrlist[j+1]=temp;
+                // var datalist=[];
+                // for(var i=0;i<data.length;i++){
+                //     datalist.push(data[i].details[0].team)
+                // }
+                // function sortarr(arrlist,dataSort){
+                //     for(i=0;i<arrlist.length-1;i++){
+                //         for(j=0;j<arrlist.length-1-i;j++){
+                //             if(arrlist[j]>arrlist[j+1]){
+                //                 var temp=arrlist[j];
+                //                 arrlist[j]=arrlist[j+1];
+                //                 arrlist[j+1]=temp;
 
-                                var sortdata=dataSort[j];
-                                dataSort[j]=dataSort[j+1];
-                                dataSort[j+1]=sortdata;
-                            }
-                        }
-                    }
-                    return dataSort;
-                }
-                $scope.users = sortarr(datalist,data);
+                //                 var sortdata=dataSort[j];
+                //                 dataSort[j]=dataSort[j+1];
+                //                 dataSort[j+1]=sortdata;
+                //             }
+                //         }
+                //     }
+                //     return dataSort;
+                // }
+                //$scope.users = sortarr(datalist,data);
+                $scope.users = data.sort(function(a,b){
+                    return a.details[0].team > b.details[0].team;
+                })
                 //aaa=sortarr(datalist,data);
                 //forLists(aaa);
             }
